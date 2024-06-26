d18f7fd6da6b414bfc6438fa0a8aedc1d0b60954
gialbanes <giovanaalbanes@gmail.com>
2024-06-26 15:39:02 -0300

    exercício interface funcional - Predicate + alterações no consumer

--- estudos/contextualizandoStreamAPI/consumer/src/ConsumerExample.java
+++ estudos/contextualizandoStreamAPI/consumer/src/ConsumerExample.java
@@ -3,8 +3,10 @@ import java.util.List;
 import java.util.function.Consumer;
 
 /**
- * Representa uma operação que aceita um argumento do tipo T(qualquer tipo) e não retorna nenhum resultado.
- * É utilizada principalmente para realizar ações, ou efeitos colaterais nos elementos do Stream sem modificar, ou
+ * Representa uma operação que aceita um argumento do tipo T(qualquer tipo) e
+ * não retorna nenhum resultado.
+ * É utilizada principalmente para realizar ações, ou efeitos colaterais nos
+ * elementos do Stream sem modificar, ou
  * retornar um valor.
  */
 
@@ -24,12 +26,7 @@ public class ConsumerExample {
         // o foreach é o responsável por imprimir 
         //quando eu passo o consumer p dentro do forEach, o numero que está como argumento no lambda é suibtituido por cada elemento de dentro do list daqui
         // o forEach recebe o Consumer; itera sobre cada elemento da lista e aplica o Consumer fornecido a cada elemento.
-        numeros.forEach(n -> { 
-                if(n % 2 == 0){
-                    System.out.println(n);
-                }
-            }
-            
-        );
+        numeros.stream().filter(n ->  n % 2 == 0)
+        .forEach(System.out::println);
     }
 }
--- estudos/contextualizandoStreamAPI/function/src/FunctionExample.java
+++ estudos/contextualizandoStreamAPI/function/src/FunctionExample.java
@@ -5,35 +5,45 @@ import java.util.stream.Collectors;
 
 public class FunctionExample {
     /**
- * Representa uma função que aceita um argumento do tipo T e retorna um resultado do tipo R.
- * É utilizada para transformar, ou mapear os elementos do Stream em outros valores, ou tipos.
- */
+     * Representa uma função que aceita um argumento do tipo T e retorna um
+     * resultado do tipo R.
+     * É utilizada para transformar, ou mapear os elementos do Stream em outros
+     * valores, ou tipos.
+     */
 
     public static void main(String[] args) {
-        //criando lista de números inteiros
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        // criando lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
-        //usando a function com expressao lambda para dobrar todos os numeros
+        // usando a function com expressao lambda para dobrar todos os numeros
         Function<Integer, Integer> dobrar = numero -> numero * 2;
 
-        //usando a funcao para dobrar todos os numeros no Stream e armazena-los em outra lista
+        // usando a funcao para dobrar todos os numeros no Stream e armazena-los em
+        // outra lista
         // a partir do stream chama o map, para mapear um valor p outro valor
         List<Integer> numerosDobrados = numeros.stream()
-            .map(dobrar) // chama a function
-            //caso eu não soubesse usar o function
-            /* .map(
-                new*
-                new Function<Integer, Integer>(){
-                    new*
-               @Override
-               public Integer apply(Integer n){
-               return n*2;
-               }
-             }
-        ) */
-            .toList();
+                .map(dobrar) // chama a function
+                // caso eu não soubesse usar o function
+                /*
+                 .map(
+                 new*
+                 new Function<Integer, Integer>(){
+                 new*
+                 
+                 @Override
+                 public Integer apply(Integer n){
+                 return n*2;
+                 }
+                 }
+                 )
+                 */
+                //se eu nao souber, e quiser usar lambda
+                /*
+                .map(n -> n*2)
+                 */
+                .toList();
 
-            //imprimir a lista de números dobrados 
-            numerosDobrados.forEach(System.out::println); //method reference 
+        // imprimir a lista de números dobrados
+        numerosDobrados.forEach(System.out::println); // method reference
     }
 }
