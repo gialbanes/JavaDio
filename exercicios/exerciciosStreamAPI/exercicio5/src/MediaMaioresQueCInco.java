3e9d4201b5db008638c4416b5d0c5bd89ec03369
DSM1 <fatec-dsm1@academico.intra>
2024-06-26 21:35:40 -0300

    exercício 5 - API

---
+++ exercicios/exerciciosStreamAPI/exercicio5/src/MediaMaioresQueCInco.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Stream;
+
+public class MediaMaioresQueCInco {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        OptionalDouble media = numeros.stream()
+        .filter(n -> n > 5)
+        .mapToDouble(Integer::doubleValue) //converte para o tipo primitivo int, pois o método sum so aceita streams de tipo primitivo
+        .average();
+
+        System.out.println("A média dos números maiores que 5 é: " + media);
+
+    }
+}
