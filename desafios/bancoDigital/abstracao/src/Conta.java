cafe9e2a59c964e0a6d426ea689bd51db8cd18b1
DSM1 <fatec-dsm1@academico.intra>
2024-06-26 22:09:12 -0300

    Desafio Banco Digital

---
+++ desafios/bancoDigital/abstracao/src/Conta.java
@@ -0,0 +1,24 @@
+public class Conta {
+    private int agencia;
+    public int getAgencia() {
+        return agencia;
+    }
+    private int numero;
+    public int getNumero() {
+        return numero;
+    }
+    private double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+        
+    }
+}
