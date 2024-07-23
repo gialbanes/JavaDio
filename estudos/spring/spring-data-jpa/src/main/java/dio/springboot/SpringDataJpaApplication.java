86d2eebd151dc73ba2087b9edcc8b15a61dfad15
gialbanes <giovanaalbanes@gmail.com>
2024-07-23 19:06:06 -0300

    alterações realizadas

--- estudos/spring/spring-data-jpa/src/main/java/dio/springboot/SpringDataJpaApplication.java
+++ estudos/spring/spring-data-jpa/src/main/java/dio/springboot/SpringDataJpaApplication.java
@@ -2,10 +2,8 @@ package dio.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("dio.springboot.model")
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
