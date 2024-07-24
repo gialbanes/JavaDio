a2531f48c9eace12ba516229fefe8ee27a87a157
gialbanes <giovanaalbanes@gmail.com>
2024-07-24 17:34:31 -0300

    security: configure adapter

--- estudos/spring/spring-security/src/main/java/dio/springboot/security/WebSecurityConfig.java
+++ estudos/spring/spring-security/src/main/java/dio/springboot/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package dio.springboot.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SuppressWarnings("deprecation")
@@ -10,6 +11,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override //agora podemos deixar nosso controller mais clean
+    protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/managers").hasAnyRole("MANAGERS")
+            .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
+            .anyRequest().authenticated().and().formLogin();
+}
 	@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { //AuthenticationManagerBuilder: vai criar uma cadeia de users; não tem nada a ver com BD
 		auth.inMemoryAuthentication()
---
+++ estudos/spring/spring-security/src/main/java/dio/springboot/security/WebSecurityConfigurerAdapter.java
@@ -0,0 +1,5 @@
+package dio.springboot.security;
+
+public class WebSecurityConfigurerAdapter {
+
+}
