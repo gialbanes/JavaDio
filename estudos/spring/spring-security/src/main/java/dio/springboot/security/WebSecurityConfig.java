bf565084224afc3ed3f9849d46b16f1005183a1b
gialbanes <giovanaalbanes@gmail.com>
2024-07-24 17:26:40 -0300

    security: gerando outros users

---
+++ estudos/spring/spring-security/src/main/java/dio/springboot/security/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package dio.springboot.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { //AuthenticationManagerBuilder: vai criar uma cadeia de users; não tem nada a ver com BD
+		auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
+                .roles("USERS")
+                .and()
+                .withUser("admin")
+                .password("{noop}master123")
+                .roles("MANAGERS");
+    }
+}
