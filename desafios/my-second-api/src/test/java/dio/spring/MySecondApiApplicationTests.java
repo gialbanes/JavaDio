fc3b059c4e12821077dc8867ab4d894f636ffab5
gialbanes <giovanaalbanes@gmail.com>
2024-07-26 18:17:00 -0300

    começando o ultimo desafio

---
+++ desafios/my-second-api/src/test/java/dio/spring/MySecondApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
