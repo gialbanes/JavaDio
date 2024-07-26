dfdb9f7681c7d171b502c7728815fc753ab649ea
gialbanes <giovanaalbanes@gmail.com>
2024-07-26 18:47:38 -0300

    algumas implementações

---
+++ desafios/my-second-api/src/main/java/dio/spring/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dio.spring.domain.repository;
+
+import dio.spring.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
